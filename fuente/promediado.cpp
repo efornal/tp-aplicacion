ae3ea675797542bdb5cbbe968a660dbf546ee5e2
christian <c.pfarher@gmail.com>
2010-05-19 18:46:33 -0300

    agrego observaciones a consultar...

--- fuente/promediado.cpp
+++ fuente/promediado.cpp
@@ -13,19 +13,21 @@ using namespace cimg_library;
 using namespace std;
 
 int main(int argc, char **argv) {
+	/*float orden = cimg_option ("-orden", 2.0, "orden del filtro butter");
+	 float frec_corte =
+	 cimg_option ("-frec_corte", 70.0, "frec. corte del filtro butter");*/
 	CImgList<double> lista;
 	CImg<double> tmp;
 	for (int i = 1; i < argc; i++) {
 		tmp.assign(argv[i]);
-		tmp.display();
-		lista.push_back(tmp);
-
+		lista.push_back(detectar_bordes(tmp, 2.0, 70.0));
 	}
-	//promedio(lista).display();
 
+	CImgDisplay disp(promedio(lista), "promediado");
 
-	/*while ((!disp.is_closed() && !disp.is_keyQ())) {
-	 //		disp.wait_all();
-	 }*/
+	while (!disp.is_closed()){
+		disp.wait();
+	}
+
 	return 0;
 }
