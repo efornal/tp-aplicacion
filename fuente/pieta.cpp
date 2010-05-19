2cbeea676511ce6d834f010ab0e9f7d2aecdc3c3
christian <c.pfarher@gmail.com>
2010-05-19 18:35:18 -0300

    cambio orde a 2.0

--- fuente/pieta.cpp
+++ fuente/pieta.cpp
@@ -13,35 +13,36 @@ using namespace cimg_library;
 using namespace std;
 
 int main(int argc, char **argv) {
-  const char *filename = cimg_option( "-f",
-				      "imagenes/la-piedad.jpg",
-				      "ruta archivo imagen" );
-  int ancho = cimg_option ( "-ancho", 20, "ancho de recuadro");
-  int alto = cimg_option ( "-alto", 20, "alto de recuadro");
-  float orden = cimg_option ("-orden", 10.0, "orden del filtro butter");
-  float frec_corte =
-    cimg_option ("-frec_corte", 70.0, "frec. corte del filtro butter");
+	const char *filename = cimg_option( "-f",
+			"imagenes/la-piedad.jpg",
+			"ruta archivo imagen" );
+	int ancho = cimg_option ( "-ancho", 20, "ancho de recuadro");
+	int alto = cimg_option ( "-alto", 20, "alto de recuadro");
+	float orden = cimg_option ("-orden", 2.0, "orden del filtro butter");
+	float frec_corte =
+			cimg_option ("-frec_corte", 70.0, "frec. corte del filtro butter");
 
-  CImgDisplay disp, disp2;
-  CImg<double> img(filename);
-  img.channel(0); //temporalmente
-  img.display(disp);
+	CImgDisplay disp, disp2;
+	CImg<double> img(filename);
+	img.channel(0); //temporalmente
+	img.display(disp);
 
-  CImgList<double> lista = segmentar(img, ancho, alto);
+	CImgList<double> lista = segmentar(img, ancho, alto);
 
-  //promedio de una lista de imagenes:
-  CImgDisplay disp_promedio(promedio(lista), "promedio de lista de imagenes");
-  //deteccion de los bordes:
-  CImgDisplay disp_bordes(detectar_bordes(img, orden, frec_corte),
-			  "bordes de imagen");
+	//promedio de una lista de imagenes:
+	CImgDisplay disp_promedio(promedio(lista), "promedio de lista de imagenes");
+	//deteccion de los bordes:
+	CImgDisplay disp_bordes(detectar_bordes(img, orden, frec_corte),
+			"bordes de imagen");
 
-  while ((!disp.is_closed() && !disp.is_keyQ())) {
-    disp.wait_all();
-  }
+	while ((!disp.is_closed() && !disp.is_keyQ())) {
+		disp.wait_all();
+	}
 
-  printf("tamanio de la lista: %d; error: %f", lista.size(),
-	 calcular_mse(lista.get_images(0,0),lista.get_images(1,1)));
+	printf("tamanio de la lista: %d; error: %f", lista.size(), calcular_mse(
+			lista.get_images(0, 0), lista.get_images(1, 1)));
 
-  lista[0].display(); lista[1].display();
-  return 0;
+	lista[0].display();
+	lista[1].display();
+	return 0;
 }
