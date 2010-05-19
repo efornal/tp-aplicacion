28e7632a051f3622ee8b5c932f046b643e5a7c8b
esteban <efornal@gmail.com>
2010-05-18 23:09:39 -0300

    funcion para segmentar imagen en cuadritos

--- fuente/pieta.cpp
+++ fuente/pieta.cpp
@@ -8,10 +8,17 @@ int main( int argc, char **argv ) {
     const char *filename = cimg_option( "-f",
                                         "imagenes/la-piedad.jpg",
                                         "ruta archivo imagen" );
-    CImgDisplay disp;
-    CImg<double> img (filename);
+    int ancho = cimg_option ( "-ancho", 20, "ancho de recuadro");
+    int alto  = cimg_option ( "-alto", 20, "alto de recuadro");
 
-    segmentar(img).display(disp);
+    CImgDisplay disp,disp2;
+    CImg<double> img (filename);
+    img.channel(0); //temporalmente
+    img.display(disp);
+
+    CImgList<double> lista = segmentar( img, ancho, alto );
+
+    lista.display(disp2);
     
     while ( (!disp.is_closed() &&  !disp.is_keyQ()) ) {
         disp.wait();
--- lib/commons.h
+++ lib/commons.h
@@ -3,6 +3,17 @@
 using namespace cimg_library;
 
 template<class T>
-CImg<T> segmentar( CImg<T> img ) {
-    return img;
+CImgList<T> segmentar( CImg<T> img, int ancho=20, int alto=20 ) {
+
+    int nro_cuadros = (img.width()/ancho) * (img.height()/alto);
+    int cont = 0;
+    CImgList<T> cuadros;
+
+    for (int y=0; y < img.height() && y+alto < img.height(); y+=alto ) {
+        for (int x=0; x < img.width() && x+ancho < img.width(); x+=ancho ) {
+            cuadros.insert( img.get_crop( x, y, x+ancho-1, y+alto-1, true ), cont++ );
+        }
+    }
+
+    return cuadros;
 }
