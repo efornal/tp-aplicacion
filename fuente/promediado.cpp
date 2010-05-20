62948eeda22eae448165c702d8b273c37f439256
esteban <efornal@gmail.com>
2010-05-19 22:44:00 -0300

    tab con 4 espacios

--- fuente/promediado.cpp
+++ fuente/promediado.cpp
@@ -13,33 +13,33 @@ using namespace cimg_library;
 using namespace std;
 
 int main(int argc, char **argv) {
-	/*float orden = cimg_option ("-orden", 2.0, "orden del filtro butter");
-	 float frec_corte =
-	 cimg_option ("-frec_corte", 70.0, "frec. corte del filtro butter");*/
-	CImgList<double> lista;
-	CImg<double> tmp;
-	CImg<double> match(argv[1]);
-	match = detectar_bordes(match, 2.0, 70.0).normalize(0,1.0);
-	for (int i = 2; i < argc; i++) {
-		tmp.assign(argv[i]);
-		lista.push_back(detectar_bordes(tmp, 2.0, 70.0).normalize(0,1.0));
-	}
-	CImg<double> prom = promedio(lista);
-	CImgList<double> lista_segmentada = segmentar(match, 40, 40);
+    /*float orden = cimg_option ("-orden", 2.0, "orden del filtro butter");
+      float frec_corte =
+      cimg_option ("-frec_corte", 70.0, "frec. corte del filtro butter");*/
+    CImgList<double> lista;
+    CImg<double> tmp;
+    CImg<double> match(argv[1]);
+    match = detectar_bordes(match, 2.0, 70.0).normalize(0,1.0);
+    for (int i = 2; i < argc; i++) {
+        tmp.assign(argv[i]);
+        lista.push_back(detectar_bordes(tmp, 2.0, 70.0).normalize(0,1.0));
+    }
+    CImg<double> prom = promedio(lista);
+    CImgList<double> lista_segmentada = segmentar(match, 40, 40);
 
-	CImgList<double> prom_segmentada = segmentar(prom, 40, 40);
-	/*	CImgList<double> p_l(prom);
-	 CImgList<double> m_l(match);*/
-	prom.print();
-	match.print();
-	printf("Error = %f\n", prom.MSE(match));
+    CImgList<double> prom_segmentada = segmentar(prom, 40, 40);
+    /*	CImgList<double> p_l(prom);
+        CImgList<double> m_l(match);*/
+    prom.print();
+    match.print();
+    printf("Error = %f\n", prom.MSE(match));
 
-	CImgDisplay disp(prom, "promediado");
-	CImgDisplay di(match, "match");
-	match.display();
-	while (!disp.is_closed()) {
-		disp.wait();
-	}
+    CImgDisplay disp(prom, "promediado");
+    CImgDisplay di(match, "match");
+    match.display();
+    while (!disp.is_closed()) {
+        disp.wait();
+    }
 
-	return 0;
+    return 0;
 }
