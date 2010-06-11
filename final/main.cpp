0dad659167ff3ea56e5ae84c1312079675dc945d
mauro <maurotorrez@gmail.com>
2010-06-10 22:30:41 -0300

    clasificador andando!!! falta comentar, voy a comer algo antes...

--- final/main.cpp
+++ final/main.cpp
@@ -5,10 +5,8 @@
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-  const char *imagen1 = cimg_option( "-img1", "./imagenes/chaco/chaco00.jpg",
+  const char *imagen1 = cimg_option( "-img1", "./imagenes/imagenesprueba/fich10.jpg",
 				     "primera imagen a comparar" );
-  const char *imagen2 = cimg_option( "-img2", "./imagenes/trenfrente/tren_frente01.jpg",
-				     "segunda imagen a comparar" );
   const char *base_dir = cimg_option( "-base", "./imagenes/base/",
 				      "directorio con el que generar la base" );
   int tamanio = cimg_option ( "-tam", 20, "lado de recuadro");
@@ -18,13 +16,21 @@ int main(int argc, char **argv) {
   int t = comp.cargar_imagenes( base_dir );
   printf( "Se han cargado %d imagenes en la base.\n", t );
 
-  comp.etiquetar_imagenes();
+  t = comp.etiquetar_imagenes();
+  printf( "Se han etiquetado %d imagenes.\n", t );
 
   t = comp.calcular_caracteristicas ();
   printf( "Se han calculado %d caracteristicas de las imagenes de la base.\n", t );
 
-  t = comp.encontrar_mas_parecida( CImg<double>( imagen1 ) );
-  printf( "La imagen mas parecida es:\n%s\n", comp.nombre( t ).c_str() );
+  t = comp.generar_prototipos();
+  if ( t==0 )
+    printf( "Se han generado los prototipos correctamente.\n" );
+
+  // t = comp.encontrar_mas_parecida( CImg<double>( imagen1 ) );
+  // printf( "La imagen mas parecida es:\n%s\n", comp.nombre( t ).c_str() );
+
+  t = comp.clasificar_imagen( CImg<double>( imagen1 ) );
+  printf( "La imagen es de la clase:\n%s\n", comp.etiqueta( t ).c_str() );
 
   return 0;
 }
