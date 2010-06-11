c13f8f089fecedf422c52b76361d8b18a6c7f59d
christian <c.pfarher@gmail.com>
2010-06-10 22:30:03 -0300

    arreglo porque explotaba, la fucnion debe devolver void

--- lib/segmentacion.h
+++ lib/segmentacion.h
@@ -122,79 +122,79 @@ vector<T> get_pos_max_acumulador(CImg<T> imagen) {
 }
 
 template<class T>
-CImg<T> normalizar( CImg<T> &imagen ) {
-    T maximo = imagen.max();
-    
-    cimg_forXY( imagen,x,y ) {
-        imagen(x,y) /= (double)maximo;
-    }
+void normalizar(CImg<T> &imagen) {
+	T maximo = imagen.max();
+
+	cimg_forXY( imagen,x,y )
+		{
+			if (maximo != 0) { //fixme: si es 0 que hacemos?
+				imagen(x, y) /= (double) maximo;
+			}
+		}
 }
 
 template<class T>
-CImg<T> obtener_maximos_acumuladores( CImg<T> imagen, 
-                                      int cantidad = 50,
-                                      int direccion = -99, 
-                                      int tolerancia = 0) {
-    vector<T> maximo_actual;
-    CImg<T> acum( cantidad*2, 1, 1, 1, 0 );
-    CImg<T> acum_rho( cantidad, 1, 1, 1, 0 );
-    CImg<T> acum_theta( cantidad, 1, 1, 1, 0 );
-    int x_con_tol_izq = 0;
-    int x_con_tol_der = 0;
+CImg<T> obtener_maximos_acumuladores(CImg<T> imagen, int cantidad = 50,
+		int direccion = -99, int tolerancia = 0) {
+	vector<T> maximo_actual;
+	CImg<T> acum(cantidad * 2, 1, 1, 1, 0);
+	CImg<T> acum_rho(cantidad, 1, 1, 1, 0);
+	CImg<T> acum_theta(cantidad, 1, 1, 1, 0);
+	int x_con_tol_izq = 0;
+	int x_con_tol_der = 0;
 
-    int M = imagen.width(), 
-        N = imagen.height();
+	int M = imagen.width(), N = imagen.height();
 
-    double max_rho = sqrt( float( pow(N, 2) + pow(M, 2) ) ),
-        step_rho   = 2. * max_rho / (N - 1), // paso en eje rho (rho=[-max_rho , max_rho])
-        step_theta = M_PI / (M - 1), // paso en eje theta (M_PI=pi) (theta=[-90,90])
-        rho, theta;
+	double max_rho = sqrt(float(pow(N, 2) + pow(M, 2))), step_rho = 2.
+			* max_rho / (N - 1), // paso en eje rho (rho=[-max_rho , max_rho])
+			step_theta = M_PI / (M - 1), // paso en eje theta (M_PI=pi) (theta=[-90,90])
+			rho, theta;
 
-    if (direccion != -99) { //busca en direccion especifica
-        int ancho = imagen.width() - 1;
-        int alto = imagen.height() - 1;
-        int medio = ancho / 2.0; // este va a ser el 0 grados
+	if (direccion != -99) { //busca en direccion especifica
+		int ancho = imagen.width() - 1;
+		int alto = imagen.height() - 1;
+		int medio = ancho / 2.0; // este va a ser el 0 grados
 
-        //posicion real del plano rho theta donde se quiere
-        x_con_tol_izq = medio + ((direccion - tolerancia) * ((ancho - medio)
-                                                             / 90.0));
+		//posicion real del plano rho theta donde se quiere
+		x_con_tol_izq = medio + ((direccion - tolerancia) * ((ancho - medio)
+				/ 90.0));
 
-        //posicion real del plano rho theta donde se quiere
-        x_con_tol_der = medio + ((direccion + tolerancia) * ((ancho - medio)
-                                                             / 90.0));
+		//posicion real del plano rho theta donde se quiere
+		x_con_tol_der = medio + ((direccion + tolerancia) * ((ancho - medio)
+				/ 90.0));
 
-        if (x_con_tol_izq < 0 || x_con_tol_izq > ancho)
-            x_con_tol_izq = 0; //para que no explote porque para -90 tira -1
+		if (x_con_tol_izq < 0 || x_con_tol_izq > ancho)
+			x_con_tol_izq = 0; //para que no explote porque para -90 tira -1
 
-        else if (x_con_tol_der < 0 || x_con_tol_der > ancho)
-            x_con_tol_der = 0; //para que no explote porque para -90 tira -1
+		else if (x_con_tol_der < 0 || x_con_tol_der > ancho)
+			x_con_tol_der = 0; //para que no explote porque para -90 tira -1
 
-        // busar los maximos
-        imagen.crop(x_con_tol_izq, 0, x_con_tol_der, alto); //ojo estoy  modificando la imagen!
+		// busar los maximos
+		imagen.crop(x_con_tol_izq, 0, x_con_tol_der, alto); //ojo estoy  modificando la imagen!
 
-    }
+	}
 
-    for (int i = 0; i < cantidad; i++) { //hallo la posicion d elos maximos
-        maximo_actual.clear();
-        maximo_actual = get_pos_max_acumulador(imagen);
+	for (int i = 0; i < cantidad; i++) { //hallo la posicion d elos maximos
+		maximo_actual.clear();
+		maximo_actual = get_pos_max_acumulador(imagen);
 
-        // lo pongo negro en el cacho de imagen para que detecte el proximo maximo
-        imagen( maximo_actual[0], maximo_actual[1] ) = 0;
-        theta   = maximo_actual[1] * step_theta - M_PI / 2;
-        rho     = maximo_actual[0] * step_rho   - max_rho; // mapea [0,N] en [-max_rho,max_rho]
-        // FIXME deben estar rho y theta normalizados a una misma escala
-        acum_rho[i]   = rho;
-        acum_theta[i] = theta;
-    }
-    normalizar( acum_rho );
-    normalizar( acum_theta );
-    int contador = 0;
-    for ( int x=0; x < acum.width()-1; x++ ) {
-        acum[x]   = acum_rho[contador]; // guardo valor de rho y theta
-        acum[x+1] = acum_theta[contador]; // guardo valor de rho y theta
-        contador++;
-    }
-    return acum;
+		// lo pongo negro en el cacho de imagen para que detecte el proximo maximo
+		imagen(maximo_actual[0], maximo_actual[1]) = 0;
+		theta = maximo_actual[1] * step_theta - M_PI / 2;
+		rho = maximo_actual[0] * step_rho - max_rho; // mapea [0,N] en [-max_rho,max_rho]
+		// FIXME deben estar rho y theta normalizados a una misma escala
+		acum_rho[i] = rho;
+		acum_theta[i] = theta;
+	}
+	normalizar(acum_rho);
+	normalizar(acum_theta);
+	int contador = 0;
+	for (int x = 0; x < acum.width() - 1; x++) {
+		acum[x] = acum_rho[contador]; // guardo valor de rho y theta
+		acum[x + 1] = acum_theta[contador]; // guardo valor de rho y theta
+		contador++;
+	}
+	return acum;
 }
 
 /* Devuelve una imagen de cant_maximos x 1 con los valores caracteristicos
@@ -210,31 +210,24 @@ CImg<T> obtener_maximos_acumuladores( CImg<T> imagen,
  * solo se aplica cuando direccion!-=-99
  * */
 template<class T>
-CImg<T> extraer_valores_caracteristicos( CImg<T> imagen, 
-                                         int cant_maximos = 50,
-                                         float umbral = 20.0, 
-                                         int direccion = -99, 
-                                         int tol_grados = 0,
-                                         bool channel0 = true, 
-                                         int q_levels = 16, 
-                                         int r_size = 100) {
-    if (channel0)
-        imagen.channel(0);
+CImg<T> extraer_valores_caracteristicos(CImg<T> imagen, int cant_maximos = 50,
+		float umbral = 20.0, int direccion = -99, int tol_grados = 0,
+		bool channel0 = true, int q_levels = 16, int r_size = 100) {
+	if (channel0)
+		imagen.channel(0);
 
-    //obtengo canal, cuantizo en 16 niveles y hago un resize
-    imagen.quantize(q_levels).resize(r_size, r_size);
+	//obtengo canal, cuantizo en 16 niveles y hago un resize
+	imagen.quantize(q_levels).resize(r_size, r_size);
 
-    //img_bordes es binaria y tiene valores entre 0 y 255...
-    CImg<T> img_bordes = aplicar_sobel<T> (imagen, umbral, true);
-    CImg<T> HOUGH_IMG_BORDES = hough_directa(img_bordes); // aplico la transformada
+	//img_bordes es binaria y tiene valores entre 0 y 255...
+	CImg<T> img_bordes = aplicar_sobel<T> (imagen, umbral, true);
+	CImg<T> HOUGH_IMG_BORDES = hough_directa(img_bordes); // aplico la transformada
 
-    HOUGH_IMG_BORDES.display();
-    // deve devolver cant_maximos de rho y tita
-    CImg<T> acums = obtener_maximos_acumuladores( HOUGH_IMG_BORDES,
-                                                  cant_maximos, 
-                                                  direccion, 
-                                                  tol_grados);
-    return acums;
+	//HOUGH_IMG_BORDES.display();
+	// deve devolver cant_maximos de rho y tita
+	CImg<T> acums = obtener_maximos_acumuladores(HOUGH_IMG_BORDES,
+			cant_maximos, direccion, tol_grados);
+	return acums;
 }
 
 template<class T>
