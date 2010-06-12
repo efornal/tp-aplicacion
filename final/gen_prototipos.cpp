bbd5006a5066988f9986985d26fa4c402e37fdce
esteban <efornal@gmail.com>
2010-06-12 17:12:59 -0300

    funcion para generar prototipos y guardarlos como imagenes

---
+++ final/gen_prototipos.cpp
@@ -0,0 +1,42 @@
+#include <CImg.h>
+#include "def_clase_2.h"
+
+using namespace cimg_library;
+
+int main(int argc, char **argv) {
+  const char *base_dir = cimg_option( "-base", "./imagenes/base/",
+				      "directorio con el que generar la base" );
+
+  int tamanio = cimg_option ( "-tam", 20, "lado de recuadro");
+
+  ComparadorImagenes<double> comp;
+
+  int t = comp.cargar_imagenes( base_dir );
+  printf( "Se han cargado %d imagenes en la base.\n", t );
+
+  t = comp.etiquetar_imagenes();
+  printf( "Se han etiquetado %d imagenes.\n", t );
+
+  t = comp.calcular_caracteristicas ();
+  printf( "Se han calculado %d caracteristicas de las imagenes de la base.\n", t );
+
+  t = comp.generar_prototipos();
+  if ( t==0 )
+    printf( "Se han generado los prototipos correctamente.\n" );
+
+
+
+  // // t = comp.encontrar_mas_parecida( CImg<double>( imagen1 ) );
+  // // printf( "La imagen mas parecida es:\n%s\n", comp.nombre( t ).c_str() );
+
+  // t = comp.clasificar_imagen( CImg<double>( imagen1 ) );
+  // printf( "La imagen es de la clase:\n%s\n", comp.etiqueta( t ).c_str() );
+
+  // vector<string> nombres; vector<int> clases;
+  // t = comp.clasificar_directorio( prueba_dir, nombres, clases );
+  // for (unsigned i=0; i<nombres.size(); i++ )
+  //   printf( "La imagen %s es de la clase:\n%s\n", nombres[i].c_str(), comp.etiqueta( clases[i] ).c_str() );
+
+
+  return 0;
+}
