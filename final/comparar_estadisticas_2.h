f309204837e0bfe3f8e2463c39133ac2fbb578b1
mauro <maurotorrez@gmail.com>
2010-06-13 23:08:30 -0300

    indentacion a 2, 80 lineas, comento

--- final/comparar_estadisticas_2.h
+++ final/comparar_estadisticas_2.h
@@ -1,8 +1,3 @@
-/*
- * extract_estadisticas_histo.cpp
- * define el extractor de características ExtractorCaracteristicasHistogramas
- */
-
 #ifndef COMPARAR_ESTADISTICAS_2_H
 #define COMPARAR_ESTADISTICAS_2_H
 
@@ -15,122 +10,127 @@
 using namespace cimg_library;
 using namespace std;
 
+/**
+ * promedio()
+ * devuelve una imagen promedio de todas las contenidas en la CImgList pasada
+ * como parámetro lista_imagenes. Si las imágenes en la lista tienen tamaños
+ * diferentes, falla (devuelve la primer imagen en la lista, en realidad).
+ * @param lista_imagenes: la lista con las imágenes a promediar.
+ * @return laa CImg<T> promedio
+ */
 template<class T>
 CImg<T> promedio(CImgList<T> lista_imagenes) {
-	/* funcion que dada una lista de imagenes retorna el promedio de la mismas
-	 * */
-	CImg<T> promediada = lista_imagenes(0);
-	unsigned int tam_lista = lista_imagenes.size();
 
-	unsigned w = lista_imagenes(0).width(), h = lista_imagenes(0).height(), d =
-			lista_imagenes(0).spectrum();
+  CImg<T> promediada = lista_imagenes(0);
+  unsigned int tam_lista = lista_imagenes.size();
 
-	for (unsigned int i = 1; i < tam_lista; i++) {
-		// valido que las imÃ¡genes a comparar tengan las mismas dimensiones ( x, y, c )
-		if (lista_imagenes(i).width() != w || lista_imagenes(i).height() != h
-				|| lista_imagenes(i).spectrum() != d)
-			return promediada; // caracoles, no explotarÃ¡ la cosa acÃ¡?
-		//->fixme: comento esto porque me patea si retornas null
-		/*	promediada += lista_imagenes(i);*/
-		cimg_forXYC(promediada,x,y,c)
-				{
-					promediada(x, y, c) += lista_imagenes[i](x, y, c);
-				}
-	}
-	cimg_forXYC(promediada,x,y,c)
-			{
-				promediada(x, y, c) /= (double) tam_lista;
-			}
+  unsigned w = lista_imagenes(0).width(),
+    h = lista_imagenes(0).height(),
+    d = lista_imagenes(0).spectrum();
 
-	return promediada;
+  for (unsigned int i = 1; i < tam_lista; i++) {
+    // valido que las imágenes tengan las mismas dimensiones (x,y,c)
+    if ( lista_imagenes(i).width() != w
+	 || lista_imagenes(i).height() != h
+	 || lista_imagenes(i).spectrum() != d )
+      return promediada;
+
+    cimg_forXYC(promediada,x,y,c) {
+	promediada(x, y, c) += lista_imagenes[i](x, y, c);
+    }
+  }
+  cimg_forXYC(promediada,x,y,c) {
+      promediada(x, y, c) /= (double) tam_lista;
+  }
+  
+  return promediada;
 }
 
 /**
- * generar_base
- * genera una imagen base con todas las imï¿½genes que encuentre en el directorio
- * pasado como parï¿½metro.
- * @param directorio directorio donde buscar las imï¿½genes para generar la base
+ * generar_base()
+ * genera una imagen base (promedio), a partir de todas las imágenes que
+ * encuentre en el directorio pasado como parámetro.
+ * @param directorio directorio donde buscar las imágenes para generar la base
  * @return base generada (sin segmentar, por ahora)
  */
 template<class T>
 CImg<T> generar_base(const char* directorio) {
-	CImgList<T> lista_imagenes;
-	CImg<T> temp;
+  CImgList<T> lista_imagenes;
+  CImg<T> temp;
 
-	// el "globbing" me permite expandir los asteriscos como cuando hago `ls ./*`
-	// acï¿½ lo que hago es expandir la expresiï¿½n (directorio)/*, y para cada una
-	// de las expansiones matcheo la expresiï¿½n regular "match", que matchea para
-	// los archivos tipo imagen: .jpg, .jpeg, .JPG, .bmp, et cetera.
+  // el "globbing" me permite expandir los asteriscos como cuando hago `ls ./*`
+  // acá lo que hago es expandir la expresión (directorio)/*, y para cada una
+  // de las expansiones matcheo la expresión regular "match", que matchea para
+  // los archivos tipo imagen: .jpg, .jpeg, .JPG, .bmp, et cetera.
 
-	// globbuf es la estructura donde se guardan los resultados del glob.
-	// me interesan 2 de sus miembros: gl_pathc, que contiene el nï¿½mero
-	// de expansiones, y gl_pathv que es un char** con las expansiones mismas
-	glob_t globbuf;
+  // globbuf es la estructura donde se guardan los resultados del glob.
+  // me interesan 2 de sus miembros: gl_pathc, que contiene el número
+  // de expansiones, y gl_pathv que es un char** con las expansiones mismas
+  glob_t globbuf;
 
-	// match contendrï¿½ la expresiï¿½n regular "compilada" en la llamada a regcomp
-	regex_t match;
+  // match contendrá la expresón regular "compilada" en la llamada a regcomp
+  regex_t match;
 
-	// compilo la expresiï¿½n regular en el 2do parï¿½metro en match,
-	// el 3er argumento le dice que ignore mayusculas/minusculas
-	// por alguna putï¿½sima razï¿½n tengo que comentar con \\ los caracteres ()?|
-	regcomp( &match, ".*\\.\\(jpe\\?g\\|bmp\\tif{1,2}\\|png\\|gif\\)$",
-			REG_ICASE );
+  // compilo la expresión regular en el 2do parámetro en match,
+  // el 3er argumento le dice que ignore mayusculas/minusculas
+  // por alguna putísima razón tengo que comentar con \\ los caracteres ()?|
+  regcomp( &match, ".*\\.\\(jpe\\?g\\|bmp\\tif{1,2}\\|png\\|gif\\)$",
+	   REG_ICASE );
 
-	// expando directorio/*
-	glob(std::string(std::string(directorio) + std::string("/*")).c_str(),
-			GLOB_MARK, NULL, &globbuf);
+  // expando directorio/*
+  glob(std::string(std::string(directorio) + std::string("/*")).c_str(),
+       GLOB_MARK, NULL, &globbuf);
 
-	// recorro todas las expansiones
-	for (unsigned i = 0; i < globbuf.gl_pathc; i++) {
-		// matcheo la expresion regular
-		if (regexec(&match, globbuf.gl_pathv[i], 0, NULL, 0) == 0) {
-			// si matchea cargo la imagen en temp y la meto al final de la lista
-			lista_imagenes.push_back(CImg<T> (globbuf.gl_pathv[i]));
-		}
-	}
+  // recorro todas las expansiones
+  for (unsigned i = 0; i < globbuf.gl_pathc; i++) {
+    // matcheo la expresion regular
+    if (regexec(&match, globbuf.gl_pathv[i], 0, NULL, 0) == 0) {
+      // si matchea cargo la imagen en temp y la meto al final de la lista
+      lista_imagenes.push_back(CImg<T> (globbuf.gl_pathv[i]));
+    }
+  }
 
-	// libero los cosos auxiliares de glob y regex
-	regfree(&match);
-	globfree(&globbuf);
+  // libero los cosos auxiliares de glob y regex
+  regfree(&match);
+  globfree(&globbuf);
 
-	// devuelvo la base generada a partir del promedio de las imagenes
-	return promedio(lista_imagenes);
+  // devuelvo la base generada a partir del promedio de las imagenes
+  return promedio(lista_imagenes);
 }
 
 /**
+ * segentar()
  * Segmenta una imagen en cuadritos de ancho por alto especificado
+ * La particion de la imagen se realiza de izq a derecha y de arriba hacia abajo
+ * en cuadros de tamaño ancho x alto. Si el tamaño de la imagen no es divisible
+ * de forma exacta (sin decimales) con el tamañoo especificado, la segmentacion
+ * omite la parte sobrante (no divisible), tanto en alto como en ancho.
  * @param CImg<T> img Imagen a segmentar
  * @param int ancho Ancho de cada cuadrito
  * @param int alto Alto de cada cuadrito
  * @return CImgList<T> Lista de cuadritos de la imagen segmentada
- *
- * La particion de la imagen se realiza de izq a derecha 
- * y de arriba hacia abajo en cuadros de tamaï¿½o ancho x alto
- * Si el tamaï¿½o de la imagen no es divisible de forma exacta 
- * (sin decimales) con el tamaï¿½o especificado para los cuadritos,
- * la segmentacion omite la parte sobrante (no divisible), tanto en alto
- * como en ancho.
  */
 template<class T>
 CImgList<T> segmentar(CImg<T> img, int ancho = 20, int alto = 20) {
-	CImgList<T> cuadros;
-	for (int y = 0; y <= img.height() && y + alto <= img.height(); y += alto) {
-		for (int x = 0; x <= img.width() && x + ancho <= img.width(); x
-				+= ancho) {
-			cuadros.push_back(img.get_crop(x, y, x + ancho - 1, y + alto - 1));
-		}
-	}
-	return cuadros;
+  CImgList<T> cuadros;
+  for (int y = 0; y <= img.height() && y + alto <= img.height(); y += alto) {
+    for (int x = 0; x <= img.width() && x + ancho <= img.width(); x
+	   += ancho) {
+      cuadros.push_back(img.get_crop(x, y, x + ancho - 1, y + alto - 1));
+    }
+  }
+  return cuadros;
 }
 
 /**
- * sumar_dim
- * agarra una imagen y la suma a lo largo de una dimensiï¿½n, i.e. si sumo
- * a lo largo de x entonces obtendrï¿½ un vector en y del mismo alto que la imagen
- * original, donde cada punto en y contendrï¿½ el valor de sum(0:ancho,y)
+ * sumar_dim()
+ * agarra una imagen y la suma a lo largo de una dimensión, i.e. si sumo
+ * a lo largo de x entonces obtendré un vector en y del mismo alto que la imagen
+ * original, donde cada punto en y contendrá el valor de sum(0:ancho,y)
  * @param imagen la imagen sobre la que calcular la suma
- * @param dimension la dimensiï¿½n a sumar, por defecto x, valores posibles: 'xXyY'
- * @return vector de la suma, con una dimensiï¿½n menos que la imagen original.
+ * @param dimension la dimensión a sumar, por defecto x,
+ *                  valores posibles: 'xXyY'
+ * @return vector de la suma, con una dimensión menos que la imagen original.
  * @TODO generalizar para sumar a lo largo de las dimensiones z y c.
  */
 template<class T>
@@ -146,7 +146,7 @@ CImg<T> sumar_dim( const CImg<T> imagen, char dimension = 'x',
     if ( promediar )
       vector_salida /= imagen.width();
   }
-  // viceversa si dimensiï¿½n es X
+  // viceversa si dimensión es X
   if (dimension == 'y' | dimension == 'Y') {
     vector_salida.assign(imagen.width(), 1, 1, 1, 0);
     cimg_forX( imagen, x ) {
@@ -158,25 +158,32 @@ CImg<T> sumar_dim( const CImg<T> imagen, char dimension = 'x',
   return vector_salida;
 }
 
-double media ( const CImg<double> &histog ) {
-  unsigned x,y;
-  double media = 0;
-  cimg_forXY(histog, x, y) {
-    media += histog(x,y)*(double)(x+y);
-  }
-  return media/(double)(histog.width()*histog.height());
-}
+/* double media ( const CImg<double> &histog ) { */
+/*   unsigned x,y; */
+/*   double media = 0; */
+/*   cimg_forXY(histog, x, y) { */
+/*     media += histog(x,y)*(double)(x+y); */
+/*   } */
+/*   return media/(double)(histog.width()*histog.height()); */
+/* } */
 
-double stddev ( const CImg<double> &histog ) {
-  unsigned x,y;
-  double stddev=0, mu = (double)media(histog)*(double)(histog.width()*histog.height());
-  cimg_forXY(histog, x, y) {
-    stddev += (double)histog(x,y)*abs((double)(x+y)-mu);
-  }
-  return stddev/(double)(histog.width()*histog.height());
-}
+/* double stddev ( const CImg<double> &histog ) { */
+/*   unsigned x,y; */
+/*   double stddev=0, 
+        mu = (double)media(histog)*(double)(histog.width()*histog.height()); */
+/*   cimg_forXY(histog, x, y) { */
+/*     stddev += (double)histog(x,y)*abs((double)(x+y)-mu); */
+/*   } */
+/*   return stddev/(double)(histog.width()*histog.height()); */
+/* } */
 
-// devuelve la mediana (coordenada-x donde se da el percentil 50)
+/**
+ * mediana()
+ * devuelve la mediana (coordenada-x donde se da el percentil 50) en el
+ * histograma pasado como parámetro histo.
+ * @param histo el histograma dato.
+ * @return el valor calculado de mediana
+ */
 template<class T>
 T mediana ( const CImg<T> & histo ) {
   T sum = histo.sum(), acum = (T)0;
@@ -188,7 +195,14 @@ T mediana ( const CImg<T> & histo ) {
   return (T)x;
 }
 
-// calcula la desviación absoluta respecto de la media M
+/**
+ * absdev()
+ * devuelve la desviación absoluta del histograma respecto de la media
+ * M (parámetro). La desviación absoluta se define como: sum(abs(x-M))/n
+ * @param histo el histograma dato
+ * @param M la media respecto de la cual calcular el histograma.
+ * @return el valor calculado de desviación absoluta.
+ */
 template<class T>
 T absdev ( const CImg<T> & histo, T M ) {
   unsigned x = 0;
@@ -199,6 +213,15 @@ T absdev ( const CImg<T> & histo, T M ) {
   return absdev/(T)histo.width();
 }
 
+/**
+ * obtener_cuadrante()
+ * dada la imagen imagen, devuelve el cuadrante 1, 2, 3, o 4, si el
+ * parámetro cuadrante es alguno de esos valores; en otro caso devuelve una
+ * copia de la imagen original.
+ * @param imagen la imagen de donde se saca el cuadrante
+ * @param cuadrante el numero de cuadrante a devolver
+ * @return el cuadrante calculado de la imagen.
+ */
 template<class T>
 CImg<T> obtener_cuadrante( const CImg<T> &imagen, int cuadrante ) {
   switch(cuadrante) {
@@ -215,14 +238,19 @@ CImg<T> obtener_cuadrante( const CImg<T> &imagen, int cuadrante ) {
 }
 
 /**
- * estadisticas_imagen
- * calcula una serie de estadï¿½sticas sobre la imagen pasada como parï¿½metro, a saber
- * media y stddev sobre la imagen entera, media y stddev sobre las sumas horizontales
- * y verticales, media y stddev sobre el histograma de la imagen entera, y sobre las
- * sumas horiz y vert.
- * @param imagen la imagen sobre la que se calcularï¿½n estas medidas
- * @return un vector horizontal CImg<double> con los valores en el orden que se
- *         mencionan ariba.
+ * estadisticas_imagen()
+ * calcula una serie de estadísticas sobre la imagen pasada como parámetro:
+ * valor medio (media aritmética), mediana y desviación absoluta del histograma;
+ * y los mismos tres valores para las sumas (perfiles) horizontal y vertical.
+ * Además, estos nueve valores se calculan para los cuatro cuadrantes de la
+ * imagen si el parámetro cuadrante vale 0. Si vale 1-4, los nueve valores se
+ * calculan sobre el cuadrante dado por el valor de cuadrante; en cualquier otro
+ * caso, los nueve valores se calculan sobre la imagen entera solamente.
+ * @param imagen la imagen sobre la que se calcularán las medidas de histograma
+ * @param cuadrante el cuadrante sobre el cual calcular, si 0 se calcula sobre
+ *                  imagen entera + 4 cdrants, si otro valor sólo imagen entera
+ * @return un vector horizontal CImg<double> con los valores calculados, de
+ *         dimensión 45 si cuadrante=0, 9 en otro caso.
  */
 template<class T>
 CImg<double> estadisticas_imagen(const CImg<T> &imagen, int cuadrante = 0) {
@@ -292,31 +320,41 @@ CImg<double> estadisticas_imagen(const CImg<T> &imagen, int cuadrante = 0) {
 }
 
 /**
- * error_estadisticas_imagen
- * calcula el MSE entre los vectores de estadï¿½sticas de dos imï¿½genes.
- * @param img1, img2 las imï¿½genes a comparar
- * @return MSE (double) entre los vectores de estadï¿½sticas de las imï¿½genes.
+ * error_estadisticas_imagen()
+ * calcula el MSE entre los vectores de estadísticas de dos imágenes.
+ * @param img1, img2 las imágenes a comparar
+ * @return MSE (double) entre los vectores de estadísticas de las imágenes.
  */
 template<class T>
-double error_estadisticas_imagen(const CImg<T> img1, const CImg<T> img2,
-		double normalize_min = 0, double normalize_max = 255) {
-	CImg<double> eimg1 = estadisticas_imagen<double> (img1);
-	CImg<double> eimg2 = estadisticas_imagen<double> (img2);
-	return eimg1.normalize(normalize_min, normalize_max).MSE(eimg2.normalize(
-			normalize_min, normalize_max));
+double error_estadisticas_imagen(const CImg<T> &img1,
+				 const CImg<T> &img2,
+				 double normalize_min = 0,
+				 double normalize_max = 255) {
+  CImg<double> eimg1 = estadisticas_imagen<double> (img1);
+  CImg<double> eimg2 = estadisticas_imagen<double> (img2);
+  return eimg1.normalize(normalize_min,
+			 normalize_max).MSE(eimg2.normalize( normalize_min,
+							     normalize_max ));
 }
 
+
+/**
+ * error_estadisticas_imagen()
+ * versión que para CImglists de error_estadisticas_imagen
+ * @return el MSE promedio entre las imágenes de las listas
+ */
 template<class T>
-double error_estadisticas_imagen(const CImgList<T> img1,
-		const CImgList<T> img2, double normalize_min = 0, double normalize_max =
-				255) {
-	double error = 0;
-	unsigned int i = 0;
-	for (i; i < img1.size(); i++) {
-		error += error_estadisticas_imagen(img1[i], img2[i], normalize_min,
-				normalize_max);
-	}
-	return error / (float) i;
+double error_estadisticas_imagen( const CImgList<T> &img1,
+				  const CImgList<T> &img2,
+				  double normalize_min = 0,
+				  double normalize_max = 255 ) {
+  double error = 0;
+  unsigned int i = 0;
+  for (i; i < img1.size(); i++) {
+    error += error_estadisticas_imagen( img1[i], img2[i],
+					normalize_min, normalize_max );
+  }
+  return error / (float) i;
 }
 
 #endif
