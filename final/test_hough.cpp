fa818f6586f708c56ae73a8e07888559b0c7e2a5
christian <c.pfarher@gmail.com>
2010-06-15 00:00:15 -0300

    agrego parametros para ponderacion

--- final/test_hough.cpp
+++ final/test_hough.cpp
@@ -7,55 +7,63 @@
 using namespace cimg_library;
 
 int main(int argc, char **argv) {
-
+  const double
+	  pon1 =
+	      atof(
+	          cimg_option("-pond_estadisticas", "2.0", "Ponderacion Met. estadisticas"));
+  const double pon2 = atof(
+	  cimg_option("-pond_hough", "0.0", "Ponderacion Met. Hough"));
   vector<string> base_dir, prueba_dir;
 
-  base_dir.push_back( "./imagenes/base1/" );
-  prueba_dir.push_back( "./imagenes/prueba1/" );
+  base_dir.push_back("./imagenes/base1/");
+  prueba_dir.push_back("./imagenes/prueba1/");
 
-  base_dir.push_back( "./imagenes/base2/" );
-  prueba_dir.push_back( "./imagenes/prueba2/" );
+  base_dir.push_back("./imagenes/base2/");
+  prueba_dir.push_back("./imagenes/prueba2/");
 
-  base_dir.push_back( "./imagenes/base3/" );
-  prueba_dir.push_back( "./imagenes/prueba3/" );
+  base_dir.push_back("./imagenes/base3/");
+  prueba_dir.push_back("./imagenes/prueba3/");
 
   vector<double> error;
 
   // ============== calculo para hough ================
 
-  for ( int i=0; i< base_dir.size(); i++ ) {
+  for (int i = 0; i < base_dir.size(); i++) {
 
-    ComparadorImagenes<double> comp;
+	ComparadorImagenes<double> comp;
 
-    int t = comp.cargar_imagenes( string(base_dir[i]).c_str()  );
-    printf( "Se han cargado %d imagenes en la base.\n", t );
+	int t = comp.cargar_imagenes(string(base_dir[i]).c_str());
+	printf("Se han cargado %d imagenes en la base.\n", t);
 
-    t = comp.etiquetar_imagenes();
-    printf( "Se han etiquetado %d imagenes.\n", t );
+	t = comp.etiquetar_imagenes();
+	printf("Se han etiquetado %d imagenes.\n", t);
 
-    t = comp.calcular_caracteristicas ();
-    printf( "Se han calculado %d caracteristicas de las imagenes de la base.\n", t );
+	t = comp.calcular_caracteristicas();
+	printf("Se han calculado %d caracteristicas de las imagenes de la base.\n",
+	    t);
 
-    comp.ponderacion(0) = 0.0; // anulamos estadisticas
-    comp.ponderacion(1) = 2.0; // ponderamos por 2 por la division
-    printf("ponderaciones: %f %f\n", comp.ponderacion(0), comp.ponderacion(1) );
+	comp.ponderacion(0) = pon1; // anulamos estadisticas
+	comp.ponderacion(1) = pon2; // ponderamos por 2 por la division
+	printf("ponderaciones: %f %f\n", comp.ponderacion(0), comp.ponderacion(1));
 
-    t = comp.generar_prototipos();
-    if ( t==0 )
-      printf( "Se han generado los prototipos correctamente.\n" );
+	t = comp.generar_prototipos();
+	if (t == 0)
+	  printf("Se han generado los prototipos correctamente.\n");
 
-    vector<string> nombres; vector<int> clases;
-    t = comp.clasificar_directorio( string( prueba_dir[i] ).c_str() , nombres, clases );
-    for (unsigned i=0; i<nombres.size(); i++ )
-      printf( "%s: %s\n", nombres[i].c_str(), comp.etiqueta( clases[i] ).c_str());
+	vector<string> nombres;
+	vector<int> clases;
+	t = comp.clasificar_directorio(string(prueba_dir[i]).c_str(), nombres,
+	    clases);
+	for (unsigned i = 0; i < nombres.size(); i++)
+	  printf("%s: %s\n", nombres[i].c_str(), comp.etiqueta(clases[i]).c_str());
 
-    double err = comp.error_clasificacion( string( prueba_dir[i] ).c_str() );
-    printf( "ERRORRR Clasificación: %f\n", err );
+	double err = comp.error_clasificacion(string(prueba_dir[i]).c_str());
+	printf("ERRORRR Clasificación: %f\n", err);
 
-    error.push_back( err );
+	error.push_back(err);
   } //end for
 
-  Gnuplot g = Gnuplot("lines","lines","xlabel","ylabel", error );
+  Gnuplot g = Gnuplot("lines", "lines", "xlabel", "ylabel", error);
 
   return 0;
 }
