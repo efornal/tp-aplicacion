66d4d27cbe87390a38f5d929dbbb79a00742c4a3
mauro <maurotorrez@gmail.com>
2010-07-27 01:59:10 -0300

    main ahora acepta las opciones -csvtrain y -csvtest (no implementado aún). comento llamadas a clasificar_directorio y error_clasificación.

--- final/main.cpp
+++ final/main.cpp
@@ -6,10 +6,14 @@ using namespace cimg_library;
 int main(int argc, char **argv) {
   const char *imagen1 = cimg_option( "-img", "./imagenes/pruebas_iniciales/fich10.jpg",
 				     "primera imagen a comparar" );
-  const char *base_dir = cimg_option( "-base", "./imagenes/base/",
+  const char *base_dir = cimg_option( "-base", "./imagenes/bases/",
 				      "directorio con el que generar la base" );
-  const char *prueba_dir = cimg_option( "-prueba", "./imagenes/prueba/",
+  const char *prueba_dir = cimg_option( "-prueba", "./imagenes/pruebas/",
 				      "directorio con imgs de prueba" );
+  const char *csv_train = cimg_option( "-csvtrain", "train.csv",
+				      "CSV de salida patrones de train" );
+  const char *csv_test = cimg_option( "-csvtest", "test.csv",
+				      "CSV de salida patrones de test" );
 
   ComparadorImagenes<double> comp;
 
@@ -22,15 +26,18 @@ int main(int argc, char **argv) {
   t = comp.calcular_caracteristicas ();
   printf( "Se han calculado %d caracteristicas de las imagenes de la base.\n", t );
 
-  comp.guardar_caracteristicas_csv( "caracts.csv" );
+  comp.guardar_caracteristicas_csv( csv_train, 0, 0 );
+  printf( "Guardado los patrones de entrenamiento en CSV en %s.\n", csv_train );
 
-  comp.ponderacion(0) =  1.0;
-  comp.ponderacion(1) = 1.5;
-  printf("ponderaciones: %f %f\n", comp.ponderacion(0), comp.ponderacion(1) );
+  // @TODO implementar exportación de patrones de test
 
-  t = comp.generar_prototipos();
-  if ( t==0 )
-    printf( "Se han generado los prototipos correctamente.\n" );
+  // comp.ponderacion(0) =  1.0;
+  // comp.ponderacion(1) = 1.5;
+  // printf("ponderaciones: %f %f\n", comp.ponderacion(0), comp.ponderacion(1) );
+
+  // t = comp.generar_prototipos();
+  // if ( t==0 )
+  //   printf( "Se han generado los prototipos correctamente.\n" );
 
   // t = comp.encontrar_mas_parecida( CImg<double>( imagen1 ) );
   // printf( "La imagen mas parecida es:\n%s\n", comp.nombre( t ).c_str() );
@@ -38,13 +45,13 @@ int main(int argc, char **argv) {
   //  t = comp.clasificar_imagen( CImg<double>( imagen1 ) );
   //  printf( "La imagen es de la clase:\n%s\n", comp.etiqueta( t ).c_str() );
 
-  vector<string> nombres; vector<int> clases;
-   t = comp.clasificar_directorio( prueba_dir, nombres, clases );
-   for (unsigned i=0; i<nombres.size(); i++ )
-     printf( "%s: %s\n", nombres[i].c_str(), comp.etiqueta( clases[i] ).c_str());
+  // vector<string> nombres; vector<int> clases;
+  //  t = comp.clasificar_directorio( prueba_dir, nombres, clases );
+  //  for (unsigned i=0; i<nombres.size(); i++ )
+  //    printf( "%s: %s\n", nombres[i].c_str(), comp.etiqueta( clases[i] ).c_str());
 
-  double err = comp.error_clasificacion( prueba_dir );
-  printf( "ERRORRR Clasificación: %f\n", err );
+  // double err = comp.error_clasificacion( prueba_dir );
+  // printf( "ERRORRR Clasificación: %f\n", err );
 
   return 0;
 }
